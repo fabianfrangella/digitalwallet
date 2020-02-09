7f3a4fe3086df71d4c54bdce11c17e491d86712b
fabianfrangella <fabianfrangella@gmail.com>
2020-02-09 15:41:01 -0300

    initial commit

---
+++ src/main/kotlin/com/digitalwallet/digitalwallet/DigitalwalletApplication.kt
@@ -0,0 +1,11 @@
+package com.digitalwallet.digitalwallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DigitalwalletApplication
+
+fun main(args: Array<String>) {
+	runApplication<DigitalwalletApplication>(*args)
+}
